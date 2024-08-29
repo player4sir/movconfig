b509918b3705ec10eb00205e920f121842f8b143
我是海军呀 <31569037+player4sir@users.noreply.github.com>
2024-08-29 12:51:37 +0800

    Update handleRequest.js

--- functions/handleRequest.js
+++ functions/handleRequest.js
@@ -9,8 +9,8 @@ exports.handler = async function (event, context) {
       { 'name': '优质源(优质需代理)', 'url': 'https://json.heimuer.xyz/api.php/provide/vod/', 'type': 'default' },
       { 'name': '优质源(有广告)', 'url': 'https://api.1080zyku.com/inc/apijson.php', 'type': '1080zyku' },
       { 'name': '超清源(有广告)', 'url': 'https://www.huyaapi.com/api.php/provide/vod/from/hym3u8/at/json', 'type': 'default' },
-      { 'name': '普通源(有广告)', 'url': 'https://360zy.com/api.php/provide/vod/at/json', 'type': 'default' },
-      { 'name': '超清源(更新稍慢,需代理)', 'url': 'https://api.haiwaikan.com/v1/vod', 'type': 'haiwaikan' }
+      // { 'name': '普通源(有广告)', 'url': 'https://360zy.com/api.php/provide/vod/at/json', 'type': 'default' },
+      // { 'name': '超清源(更新稍慢,需代理)', 'url': 'https://api.haiwaikan.com/v1/vod', 'type': 'haiwaikan' }
     ],
     'models': [
       {
@@ -24,21 +24,21 @@ exports.handler = async function (event, context) {
         'name': 'qwen1.5-14b',
         'provider': '阿里'
       },
-      {
-        'id': '@cf/google/gemma-7b-it-lora',
-        'name': 'gemma-7b-it',
-        'provider': 'google'
-      },
-      {
-        'id': '@cf/mistral/mistral-7b-instruct-v0.2-lora',
-        'name': 'mistral-7b',
-        'provider': 'mistral'
-      },
-      {
-        'id': '@cf/deepseek-ai/deepseek-math-7b-instruct',
-        'name': 'deepseek-math',
-        'provider': 'deepseek'
-      }
+      // {
+      //   'id': '@cf/google/gemma-7b-it-lora',
+      //   'name': 'gemma-7b-it',
+      //   'provider': 'google'
+      // },
+      // {
+      //   'id': '@cf/mistral/mistral-7b-instruct-v0.2-lora',
+      //   'name': 'mistral-7b',
+      //   'provider': 'mistral'
+      // },
+      // {
+      //   'id': '@cf/deepseek-ai/deepseek-math-7b-instruct',
+      //   'name': 'deepseek-math',
+      //   'provider': 'deepseek'
+      // }
     ],
     'imageModels': [
       {
