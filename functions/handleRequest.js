3ca275c2c6b3f6080c1eebf5cf3407e642d168f6
我是海军呀 <31569037+player4sir@users.noreply.github.com>
2024-09-08 00:20:18 +0800

    Update handleRequest.js

--- functions/handleRequest.js
+++ functions/handleRequest.js
@@ -5,10 +5,10 @@ exports.handler = async function (event, context) {
       "url": "https://huawei8.live/api.php/provide/vod/"
     },
     "apiList": [
-      { 'name': '优质源', 'url': 'https://huawei8.live/api.php/provide/vod/', 'type': 'default' },
-      { 'name': '优质源(优质需代理)', 'url': 'https://json02.heimuer.xyz/api.php/provide/vod/', 'type': 'default' },
-      { 'name': '优质源(有广告)', 'url': 'https://api.1080zyku.com/inc/apijson.php', 'type': '1080zyku' },
-      { 'name': '超清源(有广告)', 'url': 'https://www.huyaapi.com/api.php/provide/vod/from/hym3u8/at/json', 'type': 'default' },
+      { 'name': '源0', 'url': 'https://huawei8.live/api.php/provide/vod/', 'type': 'default' },
+      { 'name': '源1', 'url': 'https://json02.heimuer.xyz/api.php/provide/vod/', 'type': 'default' },
+      { 'name': '源2', 'url': 'https://api.1080zyku.com/inc/apijson.php', 'type': '1080zyku' },
+      { 'name': '源3', 'url': 'https://www.huyaapi.com/api.php/provide/vod/from/hym3u8/at/json', 'type': 'default' },
       // { 'name': '普通源(有广告)', 'url': 'https://360zy.com/api.php/provide/vod/at/json', 'type': 'default' },
       // { 'name': '超清源(更新稍慢,需代理)', 'url': 'https://api.haiwaikan.com/v1/vod', 'type': 'haiwaikan' }
     ],
